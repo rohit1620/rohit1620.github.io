719b0b8f580ec4cadba03f9682e90b0399c1b4b0
rohit1620 <malavrohit1620@gmail.com>
2026-03-27 18:00:23 +0530

    gsap complete

--- index.js
+++ index.js
@@ -1,3 +1,114 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+     gsap.registerPlugin(ScrollTrigger)
+  // gsap code here!
+  let gt1=gsap.timeline()
+  gt1.from(".pone",{
+    y:50,
+    opacity:0,
+    duration:2
+  })
+.from(".ptwo",{
+    y:50,
+    opacity:0,
+    duration:1,
+  
+    stagger:0.3
+  },1)
+  
+
+
+  gsap.to(".textDiv_2>img",{
+   opacity:1,
+    duration:2,
+      filter: "blur(0px)",
+    
+  })
+  
+
+  let gt3=gsap.timeline({scrollTrigger: {
+      trigger: "#about",
+      start: "top 80%",  // jab box viewport me aaye
+    //   end: "top 30%",
+    //   markers: true      // debugging ke liye
+    }})
+
+    gt3.from("#about>div>h1",{
+    y:50,
+    opacity:0,
+    duration:1,
+    
+    
+  })
+    gt3.from("#about>div>p",{
+    y:20,
+    opacity:0,
+    duration:0.5,
+    
+     
+  })
+  gt3.from(".about-right-col>p",{
+    y:50,
+    opacity:0,
+   duration:1,
+    stagger:0.5,
+   
+  })
+   gt3.from(".about-right-col>button",{
+    y:20,
+    opacity:0,
+   
+    duration:0.5
+     
+  })
+
+  let gt4=gsap.timeline({scrollTrigger: {
+      trigger: "#skills",
+      start: "top 80%",  // jab box viewport me aaye
+    //   end: "top 30%",
+    //   markers: true      // debugging ke liye
+    }})
+
+    gt4.from("#skills>div>h1",{
+        y:50,
+        opacity:0,
+        duration:1,
+    }).from("#skills>div>p",{
+ y:50,
+        opacity:0,
+        duration:0.5,
+    }).from(".skills-row>div",{
+y:50,
+        opacity:0,
+        stagger:0.3,
+        duration:3.5
+    })
+
+
+    let gt5=gsap.timeline({scrollTrigger: {
+      trigger: "#services",
+      start: "top 80%",  // jab box viewport me aaye
+    //   end: "top 30%",
+    //   markers: true      // debugging ke liye
+    }})
+
+    gt5.from("#services>div>h1",{
+        y:50,
+        opacity:0,
+        duration:1,
+    }).from("#services>div>p",{
+ y:50,
+        opacity:0,
+        duration:0.5,
+    }).from(".services-row>div",{
+y:50,
+        opacity:0,
+        stagger:0.3,
+        duration:2
+    })
+
+ })
+
+
 document.getElementById("linkdin").addEventListener("click",myFun);
 function myFun(){
     location.href="https://www.linkedin.com/in/rohit-kumar-malav-205a30229/"
